322dd2d373d5d8e6dcf443af464b129c29caceb4
谢云川 <2765868257@qq.com>
2020-08-18 13:56:30 +0800

    基初Day04

--- src/it/xie/Day_four/final_.java
+++ src/it/xie/Day_four/final_.java
@@ -7,8 +7,15 @@ import java.util.Random;
  * @Des: 1;静态常量值的初始化即当类字段的字段标注为final之后，value的值在准备阶段初始化为自动的值
  */
 public class final_ {
-    static final int p= new Random().nextInt(); // 在类加载机制的alue的值在准备阶段初始化为自动的值
+    static final int p = new Random().nextInt(); // 值在准备阶段初始化为自动的值
     // final 相比 static 会早一个阶段  当然也有书上说
-    static final int p2= 5;//在javac 编译的时候就已经把值写进去了！
-    static int i=100; // static准备阶段时数据是零值，在初始化阶段才会赋值。
-} 
+    static final int p2 = 5;//在javac 编译的时候就已经把值写进去了！
+    static int i = 100; // static准备阶段时数据是零值，在初始化阶段才会赋值。
+
+    static final double PI;
+
+    static {
+        PI = 3.14159265;
+        System.out.println("准备阶段");
+    }
+}
